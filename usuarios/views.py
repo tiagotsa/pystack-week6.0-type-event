841f7d4d1331e12d35f285f71c4182b01239b47c
Tiago Almeida <tiago.almeidat@outlook.com>
2023-04-12 00:20:35 -0300

    Autenticação de usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.urls import reverse
+from django.contrib import auth
 
 # Create your views here.
 
@@ -39,3 +40,15 @@ def cadastro(request):
 def login(request):
     if request.method == "GET":
         return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=username, password=senha)
+
+        if not user:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect(reverse('login'))
+        
+        auth.login(request, user)
+        return redirect('/evento/novo_evento/')
