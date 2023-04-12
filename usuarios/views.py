d2f03276166001982fd8288dcb5939217095c326
Tiago Almeida <tiago.almeidat@outlook.com>
2023-04-11 23:35:22 -0300

    Mostrar tela de login

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,6 +6,8 @@ from django.contrib.messages import constants
 from django.urls import reverse
 
 # Create your views here.
+
+
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
@@ -16,17 +18,24 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirmar_senha')
 
         if not senha == confirmar_senha:
-            messages.add_message(request, constants.ERROR, 'As senhas não coincidem.')
+            messages.add_message(request, constants.ERROR,
+                                 'As senhas não coincidem.')
             return redirect(reverse('cadastro'))
-        
-        #TODO: Validar força da senha
+
+        # TODO: Validar força da senha
 
         user = User.objects.filter(username=username)
 
         if user.exists():
-            messages.add_message(request, constants.ERROR, 'Usuário já existe.')
+            messages.add_message(request, constants.ERROR,
+                                 'Usuário já existe.')
             return redirect(reverse('cadastro'))
-        user = User.objects.create_user(username=username, email=email, password=senha)
-        messages.add_message(request, constants.SUCCESS, 'Usuário salvo com sucesso.')
+        user = User.objects.create_user(
+            username=username, email=email, password=senha)
+        messages.add_message(request, constants.SUCCESS,
+                             'Usuário salvo com sucesso.')
         return redirect(reverse('login'))
-    
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
