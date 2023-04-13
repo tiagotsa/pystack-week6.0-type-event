9d7d2cc2c28df49051c23db4ac10c606b2c7cddc
Tiago Almeida <tiago.almeidat@outlook.com>
2023-04-13 03:10:15 -0300

    Criado Form Evento HTML e CSS, Banco Tabelas e Relação

--- usuarios/views.py
+++ usuarios/views.py
@@ -51,4 +51,4 @@ def login(request):
             return redirect(reverse('login'))
         
         auth.login(request, user)
-        return redirect('/evento/novo_evento/')
+        return redirect('/eventos/novo_evento/')
